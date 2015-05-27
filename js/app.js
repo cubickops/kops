8c44497945bfd35c35c396f6220a38180e713810
Gexgekko <scgexx@gmail.com>
2015-05-27 17:38:24 +0200

    First ngRoute

--- js/app.js
+++ js/app.js
@@ -1,4 +1,29 @@
 var kopsApp = angular.module('kopsApp',['ngRoute']);
+
+kopsApp.config(['$routeProvider',function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: '../pages/home.html',
+			controller: 'HomeCtrl'
+		})
+		.when('/home', {
+			templateUrl: '../pages/home.html',
+			controller: 'HomeCtrl'
+		})
+		.when('/leaders', {
+			templateUrl: '../pages/leaders.html',
+			controller: 'LeadersCtrl'
+		});
+}]);
+
 kopsApp.controller('MainCtrl',['$scope',function($scope){
+	var vm = $scope;
+}]);
+
+kopsApp.controller('LeadersCtrl',['$scope',function($scope){
+	var vm = $scope;
+}]);
+
+kopsApp.controller('HomeCtrl',['$scope',function($scope){
 	var vm = $scope;
 }]);
